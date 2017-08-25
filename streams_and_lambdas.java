771cd9358160383bd5e25e4a0e31c0c6f38d513b
Dennis Follensbee <da.follensbee@gmail.com>
2017-08-25 13:44:50 -0400

    Create streams_and_lambdas.java

---
+++ streams_and_lambdas.java
@@ -0,0 +1,29 @@
+void testStream() {
+    l.add(1);
+    l.add(2);
+    l.add(3);
+
+    l2 = l.stream()
+            .filter(p -> p % 2 != 0)
+            .sorted()
+            .collect(toList());
+}
+
+void testStream2() {
+    List<Integer> numbers1 = Arrays.asList(1, 2, 3);
+    List<Integer> numbers2 = Arrays.asList(3, 4);
+    String s = "1,2,3,4,5";
+    
+    Arrays.asList(s.split(","));
+
+    List<String> pairs = numbers1.stream()
+            .flatMap(i -> numbers2.stream()
+            .map(j -> String.format("%d %d", i, j)))
+            .collect(toList());
+
+    pairs.forEach(System.out::println);
+
+    numbers1.stream()
+            .map(i -> i*i)
+            .forEach(System.out::println);
+}
